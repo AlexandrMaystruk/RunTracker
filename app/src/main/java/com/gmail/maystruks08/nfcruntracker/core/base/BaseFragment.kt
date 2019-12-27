942c7be1ba23e034b6c0d35dd6c61cdecbf69a40
Alex <maystruks08@gmail.com>
2019-12-27 17:22:28 +0200

    [OPT] Improve base fragment

--- app/src/main/java/com/gmail/maystruks08/nfcruntracker/core/base/BaseFragment.kt
+++ app/src/main/java/com/gmail/maystruks08/nfcruntracker/core/base/BaseFragment.kt
@@ -1,12 +1,19 @@
 package com.gmail.maystruks08.nfcruntracker.core.base
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import javax.annotation.Resource
 
-abstract class BaseFragment : Fragment() {
+abstract class BaseFragment(@Resource private val layout : Int) : Fragment() {
 
     var toolbarManager: ToolbarManager? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(layout, container, false)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
--- app/src/main/java/com/gmail/maystruks08/nfcruntracker/ui/fragments/RootFragment.kt
+++ app/src/main/java/com/gmail/maystruks08/nfcruntracker/ui/fragments/RootFragment.kt
@@ -1,25 +1,10 @@
 package com.gmail.maystruks08.nfcruntracker.ui.fragments
 
-
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-
 import com.gmail.maystruks08.nfcruntracker.R
 import com.gmail.maystruks08.nfcruntracker.core.base.BaseFragment
 import com.gmail.maystruks08.nfcruntracker.core.base.FragmentToolbar
 
-class RootFragment : BaseFragment() {
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_root, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-    }
+class RootFragment : BaseFragment(R.layout.fragment_root) {
 
     override fun builder(): FragmentToolbar {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
@@ -28,6 +13,5 @@ class RootFragment : BaseFragment() {
     override fun initViews() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
-
 
 }
