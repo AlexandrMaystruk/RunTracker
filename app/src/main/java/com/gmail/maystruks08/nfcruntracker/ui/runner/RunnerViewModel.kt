9b7238666d3059b4a166a905e42f871c00581335
Alex <maystruks08@gmail.com>
2020-03-26 22:02:47 +0200

    [OPT] Optimize import

--- app/src/main/java/com/gmail/maystruks08/nfcruntracker/ui/runner/RunnerViewModel.kt
+++ app/src/main/java/com/gmail/maystruks08/nfcruntracker/ui/runner/RunnerViewModel.kt
@@ -5,11 +5,8 @@ import androidx.lifecycle.viewModelScope
 import com.gmail.maystruks08.domain.entities.ResultOfTask
 import com.gmail.maystruks08.domain.entities.Runner
 import com.gmail.maystruks08.domain.exception.RunnerNotFoundException
-import com.gmail.maystruks08.domain.exception.SaveRunnerDataException
-import com.gmail.maystruks08.domain.exception.SyncWithServerException
 import com.gmail.maystruks08.domain.interactors.RunnersInteractor
 import com.gmail.maystruks08.nfcruntracker.core.base.BaseViewModel
-import com.gmail.maystruks08.nfcruntracker.ui.viewmodels.RunnerView
 import kotlinx.coroutines.launch
 import ru.terrakok.cicerone.Router
 import javax.inject.Inject
