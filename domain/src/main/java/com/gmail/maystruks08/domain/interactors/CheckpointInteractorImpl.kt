f4e0bcf959118ad28a02f721e82a392ec5e54a02
Alex <maystruks08@gmail.com>
2020-12-09 02:26:59 +0200

    [OPT] Migrate to dagger hilt

--- domain/src/main/java/com/gmail/maystruks08/domain/interactors/CheckpointInteractorImpl.kt
+++ domain/src/main/java/com/gmail/maystruks08/domain/interactors/CheckpointInteractorImpl.kt
@@ -2,11 +2,18 @@ package com.gmail.maystruks08.domain.interactors
 
 import com.gmail.maystruks08.domain.entities.TaskResult
 import com.gmail.maystruks08.domain.entities.checkpoint.Checkpoint
+import com.gmail.maystruks08.domain.entities.checkpoint.CheckpointType
 import javax.inject.Inject
 
 class CheckpointInteractorImpl @Inject constructor(): CheckpointInteractor {
 
     override suspend fun getCheckpoints(distanceId: Long): TaskResult<Exception, List<Checkpoint>> {
-        TODO("Not yet implemented")
+        return TaskResult.build {
+            listOf(
+                Checkpoint(0, "Start", CheckpointType.NORMAL),
+                Checkpoint(1, "1", CheckpointType.NORMAL),
+                Checkpoint(2, "Finish", CheckpointType.NORMAL)
+            )
+        }
     }
 }
