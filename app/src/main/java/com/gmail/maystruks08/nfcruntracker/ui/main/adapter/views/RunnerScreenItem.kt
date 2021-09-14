c893819643110d55889489bfe1c74dd3ea8dce6a
Alex <maystruks08@gmail.com>
2021-09-15 00:23:09 +0300

    [REF] Rename runner interface

---
+++ app/src/main/java/com/gmail/maystruks08/nfcruntracker/ui/main/adapter/views/RunnerScreenItem.kt
@@ -0,0 +1,8 @@
+package com.gmail.maystruks08.nfcruntracker.ui.main.adapter.views
+
+import com.gmail.maystruks08.nfcruntracker.ui.adapter.base.Item
+
+interface RunnerScreenItem : Item {
+
+    val number: String
+}
--- app/src/main/java/com/gmail/maystruks08/nfcruntracker/ui/runners/adapter/views/RunnerScreenItems.kt
+++
@@ -1,8 +0,0 @@
-package com.gmail.maystruks08.nfcruntracker.ui.runners.adapter.views
-
-import com.gmail.maystruks08.nfcruntracker.ui.adapter.base.Item
-
-interface RunnerScreenItems : Item {
-
-    val number: String
-}
